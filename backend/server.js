c3ec79fc62ab704417de939557f2321c79c309bf
Muhammad Yasir <myasir47991@gmail.com>
2024-06-23 21:07:42 +0500

    made the team controllers and added all the validators

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDatabase } from './database/databaseConnection.js';
 import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import eventRouter from './routes/event.route.js';
+import teamRouter from './routes/team.route.js';
 import {v2 as cloudinary} from 'cloudinary';
 import errorMiddleware from './middleware/error.js';
 
@@ -26,6 +27,7 @@ app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/event', eventRouter);
+app.use('/api/team', teamRouter);
 
 app.use(errorMiddleware);
 app.listen(process.env.PORT || 8000, () => {
